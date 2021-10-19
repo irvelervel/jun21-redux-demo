f4812943dfdf55e3c740bc042ab74c1aca1201e7
Stefano Casasola <ste.casasola@gmail.com>
2021-10-19 10:50:19 +0200

    Adds the redux flow to the app

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+// here we're gonna write the reducer function
+// the pure function, which doesn't mutate its arguments, which is consistent
+// and with the same input, will always emit the same output
+
+import { initialState } from '../store'
+// DRY
+// don't repeat yourself
+
+export const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux'
+import { mainReducer } from '../reducers'
+// 1) configure our redux store
+// 2) define the INITIAL STATE of our app
+
+export const initialState = {
+  count: 0,
+}
+
+// createStore tipically takes 3 arguments:
+// 1) the reducer
+// 2) the initial state of the application
+// 3) [optional] an enhancer function, today we're going to use to enable our redux devTools
+
+const configureStore = createStore(
+  mainReducer,
+  initialState,
+  // the third argument is going always to be a function, needed for activate specific middlewares,
+  // plugins or extensions
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default configureStore
